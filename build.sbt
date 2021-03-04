190fcac39b3eb1895ab5596a1526fcc813f7b84c
Adam Rabcewicz <adam.rabcewicz@ewolwe.pl>
2021-03-04 10:19:02 +0100

    simple index

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "search"
 version := "0.1"
 
 scalaVersion := "2.13.5"
+
+fork in console := true
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,27 +1,63 @@
+import Index.extractWordsFrom
+
 import java.io.File
+import scala.annotation.tailrec
 import scala.util.Try
+import scala.util.chaining.scalaUtilChainingOps
 
 object Main extends App {
   Program
-    .readFile(args)
+    .checkDir(args)
     .fold(
       println,
-      file => Program.iterate(Program.index(file))
+      Index.indexDirFiles(_).pipe(Program.iterate)
     )
+}
+
+case class Index() {
+  type Word = String
+  type FileName = String
+
+  var idx: Map[Word, Set[FileName]] =
+    Map[Word, Set[FileName]]() withDefaultValue Set.empty
+
+  def addFileLine(fileName: FileName, line: String): Unit =
+    extractWordsFrom(line).foreach(w => idx = idx + (w -> (idx(w) + fileName)))
+
+  def get(word: Word): Set[FileName] = idx(word)
+
+  def query(searchString: String): List[String] =
+    extractWordsFrom(searchString)
+      .map(get)
+      .foldLeft(Set.empty[String])(_ union _)
+      .toList
+}
+
+object Index {
+  def extractWordsFrom(line: String): Iterator[String] =
+    raw"([A-Za-z0-9]+)".r.findAllIn(line).map(_.toLowerCase)
+
+  def indexDirFiles(dir: File): Index = {
+    val idx = new Index()
+    for (f <- dir.listFiles if f.isFile) {
+      val source = scala.io.Source.fromFile(f)
+      source.getLines
+        .foreach(idx.addFileLine(f.getName, _))
+      source.close()
+    }
+    idx
+  }
 }
 
 object Program {
   import scala.io.StdIn.readLine
 
-  case class Index() // TODO: Implement this
-
   sealed trait ReadFileError
-
   case object MissingPathArg extends ReadFileError
   case class NotDirectory(error: String) extends ReadFileError
   case class FileNotFound(t: Throwable) extends ReadFileError
 
-  def readFile(args: Array[String]): Either[ReadFileError, File] = {
+  def checkDir(args: Array[String]): Either[ReadFileError, File] = {
     for {
       path <- args.headOption.toRight(MissingPathArg)
       file <- Try(new java.io.File(path))
@@ -34,13 +70,17 @@ object Program {
     } yield file
   }
 
-  // TODO: Index all files in the directory
-  def index(file: File): Index = ???
-
-  def iterate(indexedFiles: Index): Unit = {
+  @tailrec
+  def iterate(index: Index): Unit = {
     print(s"search> ")
     val searchString = readLine()
-    // TODO: Make it print the ranking of each file and its corresponding score
-    iterate(indexedFiles)
+    if (searchString == ":quit") System.exit(0)
+    else {
+      index.query(searchString) match {
+        case r if r.isEmpty => println("no matches found ")
+        case r              => println(r mkString "\n")
+      }
+    }
+    iterate(index)
   }
 }
